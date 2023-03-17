dae9a2871c48398b94d19ce58166946250290708
Samuel Rooney <the.sam.rooney@gmail.com>
2023-03-17 12:40:16 -0400

    Add print statement for Sam

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,9 +3,9 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-
     App.launch(App.class, args);
     System.out.println("Ayden");
+    System.out.println("Sam");
   }
 
   // shortcut: psvm
